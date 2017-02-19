bb71a8befe6ada8e70ceb4af2421ff737cc5c51d
Kortium <ieslondor@gmail.com>
2017-02-19 04:40:16 +0300

    gem

--- helper.rb
+++
@@ -1,8 +0,0 @@
-# Класс реализующий методы которые я не смог логически никуда распределить
-
-require 'curses'
-
-def put_at_coordinates(x = 0, y = 0, string = "")
-  Curses.setpos(y, x)
-  Curses.addstr(string)
-end
